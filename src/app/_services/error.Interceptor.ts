76577bf33a1f09d6395228f3afbc8ddc2e9075bf
Tanvi Desai <dtanvi1803@yahoo.co.in>
2019-12-07 15:59:19 +0530

    ThemeingLogingAlertifyJwt

---
+++ src/app/_services/error.Interceptor.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(
+    req: import ('@angular/common/http').HttpRequest<any>,
+    next: import ('@angular/common/http').HttpHandler
+  ): import ('rxjs').Observable<import ('@angular/common/http').HttpEvent<any>> {
+    return next.handle(req).pipe(
+        catchError(errorRes => {
+            if (errorRes.status === 401) {
+                return throwError(errorRes.statusText);
+            }
+            if (errorRes instanceof HttpErrorResponse) {       // 500 type errors
+                console.log('500 type error' + errorRes);
+                const applicationError = errorRes.headers.get('Application-Error');
+                if (applicationError) {
+                    return throwError(applicationError);
+                }
+                const serverError = errorRes.error;
+                let modalStateError = '';
+                // && serverError.errors === 'Object'
+                if (serverError.errors) {
+                    for (const key in serverError.errors) {
+                        if (serverError.errors[key]) {
+                            modalStateError += serverError.errors[key] + '\n';
+                        }
+                    }
+                }
+                return throwError(modalStateError || serverError || 'Server Error');
+            }
+
+        } )
+    );
+  }
+}
+
+export const ErrorInterceptorProvider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: ErrorInterceptor,
+    multi: true
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.Interceptor';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
-   providers: [],
+   providers: [
+      AuthService,
+      ErrorInterceptorProvider
+   ],
    bootstrap: [
       AppComponent
    ]
